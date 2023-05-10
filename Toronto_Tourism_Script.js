7adaa470f4bb176afa16320279a6b27cb755fc00
GabCalayan <95249972+GabCalayan@users.noreply.github.com>
2023-05-09 20:36:36 -0400

    Added City Wards
    City Wards and Border layer

--- Toronto_Tourism_Script.js
+++ Toronto_Tourism_Script.js
@@ -8,3 +8,36 @@ const map = new mapboxgl.Map ({
     center: [-79.347015, 43.651070],
     zoom: 10,
 });
+
+//Adding GEOJSON sources 
+map.on('load', () => { 
+
+    map.addSource('City_Wards', {
+        type:'geojson',
+        data: 'https://gabcalayan.github.io/Web_Map_1/City_Wards_Data.json',
+    });
+
+    //Visualizing the City Wards layer    
+    map.addLayer({
+        'id': 'Ward',
+        'source': 'City_Wards',
+        'type': 'fill',
+        'layout': {},
+        'paint': {
+            'fill-opacity': 0.5,
+            'fill-color': 'green'
+        }
+    });
+    //Visualizing the City Wards line borders 
+    map.addLayer({
+        'id': 'outline_1',
+        'source': 'City_Wards',
+        'type': 'line',
+        'layout':{},
+        'paint': {
+            'line-width': 3,
+            'line-color': 'black'
+        }
+    });
+    
+});
